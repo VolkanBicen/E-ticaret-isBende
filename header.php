3c7e2903444b58657eadebd56863fda4deade1b5
VolkanBicen <vbicen1@gmail.com>
2018-12-03 22:52:34 +0300

    (homa page)Site amblem ayarlanması- En çalışkan 5 üye- kategori- görev (slider) gösterimi

--- header.php
+++ header.php
@@ -1,8 +1,31 @@
 <?php 
 include 'nedmin/netting/baglan.php';
- ?>
+
+$kullanicisor=$db-> prepare("SELECT * FROM kullanici order by kullanici_id asc limit 5");
+$kullanicisor -> execute(array(
+));
+
+$kategorisor=$db-> prepare("SELECT * FROM kategori ");
+$kategorisor -> execute(array(
+));
+
+$gorevsor=$db-> prepare("SELECT * FROM gorev ");
+$gorevsor -> execute(array(
+));
+
+$hakkimizdasor=$db->prepare("SELECT * FROM hakkimizda ");
+$hakkimizdasor->execute(array(
+	
+));
+$hakkimizdacek=$hakkimizdasor->fetch(PDO::FETCH_ASSOC);
 
 
+$ayarsor=$db-> prepare("SELECT * FROM ayar");
+$ayarsor -> execute(array(
+));
+$ayarcek=$ayarsor->fetch(PDO::FETCH_ASSOC);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,19 +41,12 @@ include 'nedmin/netting/baglan.php';
 	<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
 	
 	<!-- Main Style -->
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="style.css?">
 	
 	<!-- owl Style -->
 	<link rel="stylesheet" href="css\owl.carousel.css">
 	<link rel="stylesheet" href="css\owl.transitions.css">
 	
-
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 <body>
 	<div id="wrapper">
@@ -38,12 +54,12 @@ include 'nedmin/netting/baglan.php';
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-6 col-md-4 main-logo">
-						<a href="index.php"><img src="images\logo.png" alt="logo" class="logo img-responsive"></a>
-					</div>
-					
+						<a href="index.php"><img src="images\logo.png	" alt="logo" class="logo img-responsive"></a>
 
+					</div>
 
 					<div class="col-md-8">
+
 						<div class="pushright">
 							<div class="top">
 								<a href="#" id="reg" class="btn btn-default btn-dark">Giriş Yap<span>-- yada --</span>Kayıt Ol</a>
@@ -77,117 +93,21 @@ include 'nedmin/netting/baglan.php';
 										</div>
 									</div>
 								</div>
-								<div class="srch-wrap">
-									<a href="#" id="srch" class="btn btn-default btn-search"><i class="fa fa-search"></i></a>
-								</div>
-								<div class="srchwrap">
-									<div class="row">
-										<div class="col-md-12">
-											<form class="form-horizontal" role="form">
-												<div class="form-group">
-													<label for="search" class="col-sm-2 control-label">Search</label>
-													<div class="col-sm-10">
-														<input type="text" class="form-control" id="search">
-													</div>
-												</div>
-											</form>
-										</div>
-									</div>
-								</div>
+								
 							</div>
+						</div>
+						<div class="col-sm-8 col-sm-pull-2">
+							<ul class="small-menu"><!--small-nav -->
+
+								<li><a  href="hakkimizda.php"  class="mychart" >Hakkımızda</a></li>
+								<li><a href="" class="mychart">SSS</a></li>
+								<li><a href="iletisim.php" class="mychart">İletişim</a></li>
+
+							</ul><!--small-nav -->	
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="dashed"></div>
 		</div><!--Header -->
-		<div class="main-nav"><!--end main-nav -->
-			<div class="navbar navbar-default navbar-static-top">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-10">
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
-							<div class="navbar-collapse collapse">
-								<ul class="nav navbar-nav">
-									<li><a href="index.php" class="active">AnaSayfa</a><div class="curve"></div></li>
-
-									<?php 
-									$menusor=$db->prepare("SELECT * FROM menu where menu_durum=:durum order by menu_sira ASC limit 8");
-									$menusor->execute(array(
-										'durum' => 1 
-									));
-
-									while($menucek=$menusor->fetch(PDO::FETCH_ASSOC)) 
-									{
-
-										?>
-
-										<li><a  href="content.html" class="active" >&nbsp;<?php echo $menucek["menu_ad"] ?></a><div class="curve"></div></li>
-
-										
-									
-									<?php } ?>
-
-
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-2 machart">
-							<button id="popcart" class="btn btn-default btn-chart btn-sm "><span class="mychart">Cart</span>|<span class="allprice">$0.00</span></button>
-							<div class="popcart">
-								<table class="table table-condensed popcart-inner">
-									<tbody>
-										<tr>
-											<td>
-												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-											</td>
-											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-											<td>1X</td>
-											<td>$138.80</td>
-											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-										</tr>
-										<tr>
-											<td>
-												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-											</td>
-											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-											<td>1X</td>
-											<td>$138.80</td>
-											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-										</tr>
-										<tr>
-											<td>
-												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-											</td>
-											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-											<td>1X</td>
-											<td>$138.80</td>
-											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-										</tr>
-									</tbody>
-								</table>
-								<span class="sub-tot">Sub-Total : <span>$277.60</span> | <span>Vat (17.5%)</span> : $36.00 </span>
-								<br>
-								<div class="btn-popcart">
-									<a href="checkout.htm" class="btn btn-default btn-red btn-sm">Checkout</a>
-									<a href="cart.htm" class="btn btn-default btn-red btn-sm">More</a>
-								</div>
-								<div class="popcart-tot">
-									<p>
-										Total<br>
-										<span>$313.60</span>
-									</p>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-	</div><!--end main-nav -->
+		
