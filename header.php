1bdf03a0fcf122da6bc93625d9667afa70680115
demir38 <dmrrrr.mstf@gmail.com>
2018-12-14 23:58:38 +0300

    isBende Login Şifremi Unuttum

--- header.php
+++ header.php
@@ -34,7 +34,6 @@ $sayac=$kullanicigiris -> rowCount();
 $kullanicigiriscek=$kullanicigiris->fetch(PDO :: FETCH_ASSOC);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -71,115 +70,196 @@ $kullanicigiriscek=$kullanicigiris->fetch(PDO :: FETCH_ASSOC);
 
 						<div class="pushright">
 							<div class="top">
-
-
 								<?php if($_GET['mailonay']=="false" || $_GET['durum']=="bekleonay"){?>
 									<a  class="btn btn-default btn-dark">Mail Onay Bekleniyor<span></a>
-								<?php } 
-								elseif($_GET['mailonay']=="fault"){?>
-									<a href="#" id="reg" class="btn btn-default btn-dark">Böyle bir mail bulunamadı<span></a>
-								<?php } 
+									<?php } 
+									elseif($_GET['mailonay']=="fault"){?>
+										<a href="#" id="reg" class="btn btn-default btn-dark">Böyle bir mail bulunamadı<span></a>
+										<?php } 
 
-								else {?>
-									
+										else {?>
 
-								<?php if(!isset($_SESSION['kullanici_id'])){?>
-									<a href="#" id="reg" class="btn btn-default btn-dark">Giriş Yap<span>-- yada --</span>Kayıt Ol</a>
-								<?php } 
-								else {?>
-									<a href="profil.php" class="btn btn-default btn-dark"><?php echo
-									$kullanicigiriscek['kullanici_ad']?><span> </span><?php echo
-									$kullanicigiriscek['kullanici_soyad'] ?></a>
-									<a href="logout.php" name="logout" title="Logout" class="btn btn-default btn-dark">Çıkış</a>
-									
-								<?php } ?>
-								<?php } ?>
 
-								<div class="regwrap">
-									<div class="row">
-										<div class="col-md-6 regform">
-											<div class="title-widget-bg">
-												<div class="title-widget">Giriş Yap</div>
+											<?php if(!isset($_SESSION['kullanici_id'])){?>
+												<a href="#" id="reg" class="btn btn-default btn-dark">Giriş Yap<span>-- yada --</span>Kayıt Ol</a>
+											<?php } 
+											else {?>
+												<a href="profil.php" class="btn btn-default btn-dark"><?php echo
+												$kullanicigiriscek['kullanici_ad']?><span> </span><?php echo
+												$kullanicigiriscek['kullanici_soyad'] ?></a>
+												<a href="logout.php" name="logout" title="Logout" class="btn btn-default btn-dark">Çıkış</a>
+
+											<?php } ?>
+										<?php } ?>
+
+										<div class="regwrap" id="a">
+											<div class="row">
+												<div class="col-md-6 regform">
+													<div class="title-widget-bg">
+														<div class="title-widget">Giriş Yap</div>
+													</div>
+													<form action="nedmin/netting/islem.php" method="POST" role="form">
+														<div class="form-group">
+															<input type="text" class="form-control" name="kullanici_mail" id="username" placeholder="Kullanıcı Adınız(Mail Adresiniz)">
+														</div>
+														<div class="form-group">
+															<input type="password" class="form-control" 
+															name="kullanici_password" id="password" placeholder="Şifreniz">
+														</div>
+														<div>
+															<img src="guvenlik.php" alt=""/>
+															<br><br>
+														</div>
+														<div>
+															<input class="form-control" type="text" name="guvenlik_kod" autocomplete="off" placeholder="Güvenlik Kodunu Giriniz.">
+														</div><br>
+														<div class="form-group">
+															<button type="submit" name="kullanicigiris" class="btn btn-default btn-red btn-sm">Giriş</button>
+														</div>
+														<p class="change_link">
+															<?php 
+															if ($_GET['durum']=="no") {
+
+																?>
+
+																<b style="color:red;">Kullanıcı bulunamadı</b>
+
+																<?php 
+
+															} elseif ($_GET['durum']=="exit") {
+
+																?>
+
+																<b style="color:green;">Başarıyla çıkış yaptınız</b>
+
+																<?php 
+
+															}
+															elseif ($_GET['durum']=="eror") {
+																?>
+
+																<b style="color:red;">Güvenlik kodu hatalı</b>
+
+																<?php 
+
+															}
+
+															?>
+
+														</p>
+
+													</form>
+												</div>
+												<div class="col-md-6">
+													<div class="title-widget-bg">
+														<div class="title-widget">Kayıt Ol</div>
+													</div>
+													<p>
+														Kayıt OL
+													</p>
+													<a href="register"><button class="btn btn-default btn-yellow">Şimdi Kayıt Ol</button></a>
+													<button  class="btn btn-default btn-yellow" data-toggle="modal" data-target="#sifremiunuttum" type="submit" name="musterigiris" onclick="on()">Şifremi Unuttum</button>
+
+												</div>
 											</div>
-											<form action="nedmin/netting/islem.php" method="POST" role="form">
-												<div class="form-group">
-													<input type="text" class="form-control" name="kullanici_mail" id="username" placeholder="Kullanıcı Adınız(Mail Adresiniz)">
-												</div>
-												<div class="form-group">
-													<input type="password" class="form-control" 
-													name="kullanici_password" id="password" placeholder="Şifreniz">
-												</div>
-												<div>
-													<img src="guvenlik.php" alt=""/>
-													<br><br>
-												</div>
-												<div>
-													<input class="form-control" type="text" name="guvenlik_kod" autocomplete="off" placeholder="Güvenlik Kodunu Giriniz.">
-												</div><br>
-												<div class="form-group">
-													<button type="submit" name="kullanicigiris" class="btn btn-default btn-red btn-sm">Giriş</button>
-												</div>
-
-												<p class="change_link">
-													<?php 
-													if ($_GET['durum']=="no") {
-
-														?>
-
-														<b style="color:red;">Kullanıcı bulunamadı</b>
-
-														<?php 
-
-													} elseif ($_GET['durum']=="exit") {
-
-														?>
-
-														<b style="color:green;">Başarıyla çıkış yaptınız</b>
-
-														<?php 
-
-													}
-													elseif ($_GET['durum']=="eror") {
-														?>
-
-														<b style="color:red;">Güvenlik kodu hatalı</b>
-
-														<?php 
-
-													}
-
-													?>
-
-												</p>
-												
-											</form>
 										</div>
-										<div class="col-md-6">
-											<div class="title-widget-bg">
-												<div class="title-widget">Kayıt Ol</div>
-											</div>
-											<p>
-												Kayıt OL
-											</p>
-											<a href="register"><button class="btn btn-default btn-yellow">Şimdi Kayıt Ol</button></a>
-										</div>
+
+
 									</div>
 								</div>
-								
+								<div class="col-sm-8 col-sm-pull-2">
+									<ul class="small-menu"><!--small-nav -->
+
+										<li><a href="gorevler.php" class="mychart">Görevler</a></li>
+										<li><a  href="hakkimizda.php"  class="mychart" >Hakkımızda</a></li>
+										<li><a href="iletisim.php" class="mychart">İletişim</a></li>
+
+									</ul><!--small-nav -->	
+								</div>
 							</div>
 						</div>
-						<div class="col-sm-8 col-sm-pull-2">
-							<ul class="small-menu"><!--small-nav -->
+					</div>
+					<div class="dashed"></div>
 
-								<li><a href="gorevler.php" class="mychart">Görevler</a></li>
-								<li><a  href="hakkimizda.php"  class="mychart" >Hakkımızda</a></li>
-								<li><a href="iletisim.php" class="mychart">İletişim</a></li>
+				</div><!--Header -->
+				<!-- Modal Başlangıç -->
 
-							</ul><!--small-nav -->	
+
+				<div class="modal fade" id="sifremiunuttum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h4 class="modal-title" id="exampleModalLabel">Şifre Sıfırlama</h4>
+
+							</div>
+							<div class="modal-body">
+								<form action="sifremi_unuttum.php" method="POST">
+									<div class="form-group">
+										<p><b>Uyarı:</b> Girdiğiniz mail adresi kayıtlarımızda varsa şifreniz mail adresinize gönderilecektir.</p>
+									</div>
+									<div class="form-group">
+										<label for="recipient-name" class="col-form-label">Mail Adresiniz:</label>
+										<input type="email" class="form-control" name="kullanici_mail" id="recipient-name">	
+									</div>
+
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="off()">Kapat</button>
+									<button type="submit" name="sifremiunuttum1" class="btn btn-primary">Şifre Talep Et</button>
+									<?php 
+									if ($_GET['durum']=="kullaniciyok") {
+
+										?>
+										<?php 
+										$mm        = "Kullanıcı Bulunamdı";
+										$mesaj     = "<script>alert('$mm')</script>";
+										echo $mesaj;
+										?>
+
+										<?php 
+
+									} elseif ($_GET['durum']=="basarili") {
+
+										?>
+
+										<?php 
+										$mm        = "Şifre Talebiniz Alınmıştır Lütfen Mailinizi Kontrol Ediniz.";
+										$mesaj     = "<script>alert('$mm')</script>";
+										echo $mesaj;
+										?>
+
+										<?php 
+
+									}
+									elseif ($_GET['durum']=="Şifre Talebiniz Başarısız Oldu. Lütfen Tekrar Deneyiniz..!!!") {
+										?>
+
+										<?php 
+										$mm        = "Kullanıcı Bulunamdı";
+										$mesaj     = "<script>alert('$mm')</script>";
+										echo $mesaj;
+										?>
+
+										<?php 
+
+									}
+
+									?>
+
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="dashed"></div>
-		</div><!--Header -->
-		
+				<script type="text/javascript">
+
+
+					function on(){
+						document.getElementById("a").style.display = "none";
+					}
+					function off(){
+						document.getElementById("a").style.display = "block";
+					}
+
+				</script>
+<!-- Modal Bitiş -->
---
+++ sifremi_unuttum.php
@@ -0,0 +1,95 @@
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+ob_start();
+session_start();	
+date_default_timezone_set('Europe/Istanbul');
+include 'nedmin/netting/baglan.php';
+
+if(isset($_POST['sifremiunuttum1'])){
+
+	
+	$kullanicisor=$db->prepare("SELECT * FROM kullanici where kullanici_mail=:mail");
+	$kullanicisor->execute(array(
+		'mail'=>  $_POST['kullanici_mail']
+	));
+	$say=$kullanicisor->rowCount();
+	$kullanici_mail=$_POST['kullanici_mail'];
+
+	if ($say==0) {
+		
+		Header("Location:index.php?durum=kullaniciyok");
+	}else{
+		$kullanicicek=$kullanicisor->fetch(PDO::FETCH_ASSOC);
+
+		$uretilensifre=uniqid();
+		$sifrekaydet=md5($uretilensifre);
+		//Veritabanı kaydı
+
+		$sifreguncelle=$db->prepare("UPDATE kullanici SET 
+
+			kullanici_password=:kullanici_password
+
+			WHERE kullanici_mail='$kullanici_mail'");
+
+		$update=$sifreguncelle->execute(array(
+
+			'kullanici_password' => $sifrekaydet
+		));
+
+
+		//veri tabanı kaydı bitiş
+
+
+	//Mail gonderim başlat
+		$yenisifre="Yeni Sifreniz :".$uretilensifre;
+
+		
+
+		$mail = new PHPMailer();
+
+		$mail->isSMTP(); 
+		$mail->CharSet="SET NAMES UTF8";   
+		$mail->SMTPKeepAlive = true;
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = 'ssl'; 
+		$mail->Port = 465; 
+		$mail->Host = "smtp.gmail.com";
+		$mail->Username = "vbicen1@gmail.com";
+		$mail->Password = "@@v0lkan-6606";
+		$mail->setFrom("vbicen1@gmail.com");
+		$mail->addAddress($kullanici_mail);
+		$mail->isHTML(true);
+		$mail->Subject = "Sifre Yenileme Talebi";
+		$mail->Body = $yenisifre;
+
+		if($mail->Send()){
+
+			header("Location:index.php?durum=basarili");
+			
+
+		}else {
+
+			header("Location:index.php?durum=basarisiz");
+			
+
+		}
+	//Mail Gonderim Bitir
+
+	}
+
+
+
+
+}
+?>
