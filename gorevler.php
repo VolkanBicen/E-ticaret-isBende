e4c0a4ad95199f9324c3ca15595438b5a8dbcfac
demir38 <dmrrrr.mstf@gmail.com>
2018-12-20 23:51:04 +0300

    login isbende kategoriler sayfası görev çekme iyileştirme

--- gorevler.php
+++ gorevler.php
@@ -2,40 +2,55 @@
 	include 'header.php';
 	date_default_timezone_set('Europe/Istanbul');
 	$tarih= date('Y-m-d');
-	$gorevtarihsor=$db->prepare("SELECT * FROM gorev where gorev_bitTarih >:tarih ");
-	$gorevtarihsor->execute(array(
-		'tarih'=> $tarih
-	));
-
-	$gorevtarihbitsor=$db->prepare("SELECT * FROM gorev where gorev_bitTarih <:tarih ");
-	$gorevtarihbitsor->execute(array(
-		'tarih'=> $tarih
-	));
-if (isset($_GET['sef'])) {
+	
+	if (isset($_GET['sef'])) {
 		date_default_timezone_set('Europe/Istanbul');
 		$tarih1= date('Y-m-d');
-		$kategorigorevsor=$db->prepare("SELECT * FROM kategori where kategori_ad=:ad");
+		$kategorigorevsor=$db->prepare("SELECT * FROM kategori where kategori_id=:id");
 		$kategorigorevsor->execute(array(
-			'ad' => $_GET['sef']
+			'id' => $_GET['sef']
 		));
 		$kategorgorevicek=$kategorigorevsor->fetch(PDO::FETCH_ASSOC);
 		$kategori_ad=$kategorgorevicek['kategori_id'];
 
-		
-		
+		$gorevkategorisorbastar=$db->prepare("SELECT * FROM gorev where gorev_kategori=:gorev_kategori and gorev_bitTarih >=:tarih ");
+		$gorevkategorisorbastar->execute(array(
+			'gorev_kategori' => $kategori_ad,
+			'tarih'=> $tarih1
+
+		));
+		$gorevkategorisorbittar=$db->prepare("SELECT * FROM gorev where gorev_kategori=:gorev_kategori and gorev_bitTarih <=:tarih ");
+		$gorevkategorisorbittar->execute(array(
+			'gorev_kategori' => $kategori_ad,
+			'tarih'=> $tarih1
+		));
+
+
+
 
 	}else{
-		
+		date_default_timezone_set('Europe/Istanbul');
+		$tarih= date('Y-m-d');
+		$gorevkategorisorbastar=$db->prepare("SELECT * FROM gorev where gorev_bitTarih >=:tarih ");
+		$gorevkategorisorbastar->execute(array(
+			'tarih'=> $tarih
+		));
+		$gorevkategorisorbittar=$db->prepare("SELECT * FROM gorev where gorev_bitTarih <=:tarih ");
+		$gorevkategorisorbittar->execute(array(
+			'tarih'=> $tarih
+		));
+
 	}
 
-	
+
+
 	?>
 
 
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12"><!--Main content-->
-				
+
 				<div class="col-md-3"><!--Main content-->
 					<div class="title" style="">
 						<font face="Times New Roman">
@@ -47,7 +62,7 @@ if (isset($_GET['sef'])) {
 									<?php 
 									while($kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC)) 
 										{?>
-											<li class="lastone"><a href="gorevler-<?=($kategoricek["kategori_ad"])?>"><?php echo  $kategoricek['kategori_ad'] ?></a></li>
+											<li class="lastone"><a href="gorevler-<?=($kategoricek["kategori_id"])?>"><?php echo  $kategoricek['kategori_ad'] ?></a></li>
 											<?php 
 										}
 										?>
@@ -77,73 +92,92 @@ if (isset($_GET['sef'])) {
 											</div>
 											<div class="row prdct"><!--Products-->
 												<?php 
-												while($gorevtarihcek=$gorevtarihsor->fetch(PDO::FETCH_ASSOC)) 
+												while($gorevkategoricekbastar=$gorevkategorisorbastar->fetch(PDO::FETCH_ASSOC)) 
 													{?>
 														<div class="col-md-4">
 															<div class="productwrapg">
+
 																<font face="Times New Roman">
-																	<span ;class="smalltitle21" ><a href="teklif.php?id=<?php echo $gorevtarihcek['gorev_id']?>" style="color:ForestGreen;font-size:18px;font-weight: bold;" >Bütçe = <?php echo  $gorevtarihcek['gorev_butce'] ?>,00 ₺</a> </span></font>
+																	<span class="smalltitle21" ><a href="product.htm" style="color:ForestGreen;font-size:18px;font-weight: bold;" >Bütçe = <?php echo  $gorevkategoricekbastar['gorev_butce'] ?>,00 ₺</a> </span></font>
+
+
 
 																	<font face="Times New Roman">
-																		<span ;class="smalltitle21"><a href="teklif.php?id=<?php echo $gorevtarihcek['gorev_id']?>" style="font-weight: bold;font-size:15px;color: black"><br><?php echo  $gorevtarihcek['gorev_baslik'] ?></a> </span></font>
+																		<span class="smalltitle21"><a href="product.htm" style="font-weight: bold;font-size:15px;color: black"><br><?php echo  $gorevkategoricekbastar['gorev_baslik'] ?></a> </span></font>
+
 																		<font face="Times New Roman">
-																			<span ;class="smalltitle21"><a  href="teklif.php?id=<?php echo $gorevtarihcek['gorev_id']?>" style="font-size:17px;color:#343a40"><br><br><?php echo  substr($gorevtarihcek['gorev_detay'] , 0, 100)?>...</a> </span></font>
+																			<span ;class="smalltitle21"><a  href="product.htm" style="font-size:17px;color:#343a40"><br><?php echo  substr($gorevkategoricekbastar['gorev_detay'] , 0, 100)?>...</a> </span></font>
+
+																			<font face="Times New Roman">
+																				<span class="smalltitle21" ><a href="product.htm" style="color:ForestGreen;font-size:13px;font-weight: bold;position: absolute;bottom:10%;" >Bitiş Tarihi :  <?php echo  $gorevkategoricekbastar['gorev_bitTarih'] ?></a> </span></font>
+
+
+																			</div>
+
 																		</div>
-																	</div>
-																	<?php 
-																}
-																?>
-															</div>
-															<div class="row">
-																<div class="col-lg-12 text-center">
-																	<button class="btn btn-light border border-dark" data-len="6" data-loadmore="/ajax/loadmore/finishedtask" data-target="#finish-task-list-container" data-page="2">Daha Fazla Göster</button>
-																</div>
-															</div>
-															<div class="title" style="">
-																<font face="Times New Roman">
-																	<center>
-																		<div class="title"><br> <b>Bu Ayda Yapılan Görevler</b></div></center></font>
-																		<br>
-																	</div>
-
-																	<div class="row prdct"><!--Products-->
 																		<?php 
-																		while($gorevtarihbitcek=$gorevtarihbitsor->fetch(PDO::FETCH_ASSOC)) 
-																			{?>
-																				<div class="col-md-4">
-																					<div class="productwrapg">
+																	}
+																	?>
+																</div>
+																<div class="row">
+																	<div class="col-lg-12 text-center">
+																		<button class="btn btn-light border border-dark" data-len="6" data-loadmore="/ajax/loadmore/finishedtask" data-target="#finish-task-list-container" data-page="2">Daha Fazla Göster</button>
+																	</div>
+																</div>
+																<div class="title" style="">
+																	<font face="Times New Roman">
+																		<center>
+																			<div class="title"><br> <b>Bu Ayda Yapılan Görevler</b></div></center></font>
+																			<br>
+																		</div>
 
-																						<font face="Times New Roman">
-																							<span ;class="smalltitle21" ><a style="color:ForestGreen;font-size:18px;font-weight: bold;" >Bütçe = <?php echo  $gorevtarihbitcek['gorev_butce'] ?>,00 ₺</a> </span></font>
+																		<div class="row prdct"><!--Products-->
+																			<?php 
+																			while($gorevkategoricekbittar=$gorevkategorisorbittar->fetch(PDO::FETCH_ASSOC)) 
+																				{?>
+																					<div class="col-md-4">
+																						<div class="productwrapg">
 
 																							<font face="Times New Roman">
-																								<span ;class="smalltitle21"><a style="font-weight: bold;font-size:15px;color: black"><br><?php echo  $gorevtarihbitcek['gorev_baslik'] ?></a> </span></font>
+																								<span ;class="smalltitle21" ><a href="product.htm" style="color:ForestGreen;font-size:18px;font-weight: bold;" >Bütçe = <?php echo  $gorevkategoricekbittar['gorev_butce'] ?>,00 ₺</a> </span></font>
 
 																								<font face="Times New Roman">
-																									<span ;class="smalltitle21"><a  style="font-size:17px;color:#343a40"><br><br><?php echo  substr($gorevtarihbitcek['gorev_detay'] , 0, 80)?>...</a> </span></font>
+																									<span ;class="smalltitle21"><a href="product.htm" style="font-weight: bold;font-size:15px;color: black"><br><?php echo  $gorevkategoricekbittar['gorev_baslik'] ?></a> </span></font>
 
 
+																									<font face="Times New Roman">
+																										<span ;class="smalltitle21"><a  href="product.htm" style="font-size:17px;color:#343a40"><br><br><?php echo  substr($gorevkategoricekbittar['gorev_detay'] , 0, 100)?>...</a> </span></font>
+
+																										<font face="Times New Roman">
+																											<span class="smalltitle21" ><a href="product.htm" style="color:ForestGreen;font-size:13px;font-weight: bold;position: absolute;bottom:10%;" >Bitiş Tarihi :  <?php echo  $gorevkategoricekbittar['gorev_bitTarih'] ?></a> </span></font>
+
+
+
+
+
+
+
+																										</div>
+																									</div>
+																									<?php 
+																								}
+																								?>
+																							</div>
+
+																							<div class="row">
+																								<div class="col-lg-12 text-center">
+																									<button class="btn btn-light border border-dark" data-len="6" data-loadmore="/ajax/loadmore/finishedtask" data-target="#finish-task-list-container" data-page="2">Daha Fazla Göster</button>
 																								</div>
 																							</div>
-																							<?php 
-																						}
-																						?>
-																					</div>
-
-																					<div class="row">
-																						<div class="col-lg-12 text-center">
-																							<button class="btn btn-light border border-dark" data-len="6" data-loadmore="/ajax/loadmore/finishedtask" data-target="#finish-task-list-container" data-page="2">Daha Fazla Göster</button>
-																						</div>
-																					</div>
 
 
-																				</div><!--Products-->
-																				<div class="spacer"></div>
-																			</div><!--Main content-->
+																						</div><!--Products-->
+																						<div class="spacer"></div>
+																					</div><!--Main content-->
 
-																		</div>
-																	</div>
-																	<br>
-																	<br>
-																	<br>
-																	<?php include 'footer.php'; ?>
+																				</div>
+																			</div>
+																			<br>
+																			<br>
+																			<br>
+																			<?php include 'footer.php'; ?>
--- nedmin/netting/baglan.php
+++ nedmin/netting/baglan.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-	$db=new PDO("mysql:host=localhost;dbname=isbende;charset=utf8",'root','123456789');
+	$db=new PDO("mysql:host=localhost;dbname=isbende2;charset=utf8",'root','12345678');
 	//echo"basarılı";
 
 } catch (PDOExpection $e) {
